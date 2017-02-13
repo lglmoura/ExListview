3e112f89281a16c88b14497f6eafde5a15e84f21
Luiz Gustavo L Moura <lglmoura@gmail.com>
2017-02-13 14:48:43 -0200

    Criando a classe de Modelo

---
+++ app/src/main/java/com/example/lglmoura/exlistview/model/Escola.java
@@ -0,0 +1,42 @@
+package com.example.lglmoura.exlistview.model;
+
+/**
+ * Created by lglmoura on 13/02/17.
+ */
+
+public class Escola {
+
+    private String nome;
+    private int imagem; // vai armazenar o identificador do recurso
+    private String endereco;
+
+    public Escola(String nome, String endereco, int imagem) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.imagem = imagem;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(int imagem) {
+        this.imagem = imagem;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
