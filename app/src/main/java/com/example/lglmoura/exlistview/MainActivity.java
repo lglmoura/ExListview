e1cdff4afe8eb57a01761b76604a3474925e92b7
Luiz Gustavo L Moura <lglmoura@gmail.com>
2017-02-13 14:02:48 -0200

    Alterando codigo java

--- app/src/main/java/com/example/lglmoura/exlistview/MainActivity.java
+++ app/src/main/java/com/example/lglmoura/exlistview/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.lglmoura.exlistview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lista = (ListView) findViewById(R.id.lvEquipes);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,
+                R.array.equipes, android.R.layout.simple_list_item_1);
+
+        lista.setAdapter(adapter);
     }
 }
